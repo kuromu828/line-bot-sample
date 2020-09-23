017497ba42ff77eeab74011d84c52e81997a278f
souma3332 <nsouma321@gmail.com>
2020-09-23 06:22:41 +0000

    aaa

--- parrot.py
+++ parrot.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, abort
-import os
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -8,20 +7,17 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage,ImageSendMessage,
 )
+import os
 
 app = Flask(__name__)
 
+#環境変数取得
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
 YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
-
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
-
-@app.route("/")
-def hello_world():
-    return "hello world!"
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -40,12 +36,67 @@ def callback():
 
     return 'OK'
 
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+
+    if event.message.text=="HEY！":
+        url ='https://i.ytimg.com/vi/0YSxmocFCJw/maxresdefault.jpg'
+        line_bot_api.reply_message(
+            event.reply_token,[
+                TextSendMessage(text=event.message.text+"!!!!!!!!!!"),
+                ImageSendMessage(url, url), 
+            ]
+        )
+    elif event.message.text=="2001/03/21":
+        url ='https://i.ytimg.com/vi/0YSxmocFCJw/maxresdefault.jpg'
+        line_bot_api.reply_message(
+            event.reply_token,[
+                TextSendMessage(text=event.message.text+"!!!!!!!!!!"),
+                ImageSendMessage(url, url), 
+            ]
+        )
+    elif event.message.text=="String taste Lollipop":
+        url ='https://i.ytimg.com/vi/0YSxmocFCJw/maxresdefault.jpg'
+        line_bot_api.reply_message(
+            event.reply_token,[
+                TextSendMessage(text=event.message.text+"!!!!!!!!!!"),
+                ImageSendMessage(url, url), 
+            ]
+        )
+    elif event.message.text=="一覧表":
+        url ='https://i.ytimg.com/vi/0YSxmocFCJw/maxresdefault.jpg'
+        line_bot_api.reply_message(
+            event.reply_token,[
+                ImageSendMessage(url, url), 
+            ]
+        )
+    elif event.message.text=="Different_world":
+        url ='https://i.ytimg.com/vi/0YSxmocFCJw/maxresdefault.jpg'
+        line_bot_api.reply_message(
+            event.reply_token,[
+                TextSendMessage(text=event.message.text+"!!!!!!!!!!"),
+                ImageSendMessage(url, url), 
+            ]
+        )
+    elif event.message.text=="giggle":
+        url ='https://i.ytimg.com/vi/0YSxmocFCJw/maxresdefault.jpg'
+        line_bot_api.reply_message(
+            event.reply_token,[
+                TextSendMessage(text=event.message.text+"!!!!!!!!!!"),
+                ImageSendMessage(url, url), 
+            ]
+        )  
+    elif "肥田彩花" in event.message.text:
+        line_bot_api.reply_message(
+            event.reply_token,[
+                TextSendMessage("ずっと幸せを願う人"),
+            ]
+        )  
+    else:
+        print("そんなコマンドはない")
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT"))
+#    app.run()
+    port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
