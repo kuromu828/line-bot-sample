7914e9e474fd6781edf715f6eced1c4345555cf2
souma3332 <nsouma321@gmaill.com>
2020-12-09 03:07:43 +0000

    aaa

--- parrot.py
+++ parrot.py
@@ -76,7 +76,6 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token,[
                 TextSendMessage(""),
-                ImageSendMessage(./static/images/ダウンロード(1).jpeg)
             ]
         )
     elif event.message.text=="求人":
