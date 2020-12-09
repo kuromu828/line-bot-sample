62e64fcf24a06d5ef9bd04a58034a3fabab8c965
souma3332 <nsouma321@gmaill.com>
2020-12-09 02:55:58 +0000

    aaa

--- parrot.py
+++ parrot.py
@@ -56,7 +56,12 @@ def handle_message(event):
     elif event.message.text=="従業員名簿":
         line_bot_api.reply_message(
             event.reply_token,[
-                TextSendMessage(""),
+                TextSendMessage("代表"),
+                TextSendMessage("https://www.instagram.com/mako41443106/"),
+                TextSendMessage("売上No1.桜木NAOTO"),
+                TextSendMessage("https://www.instagram.com/naoto_sakuragi/"),
+                TextSendMessage("クロム"),
+                TextSendMessage("https://www.instagram.com/kuromu960_clubdna/"),
             ]
         )
     elif event.message.text=="道案内":
